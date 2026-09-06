9315f71f67ab36c8017b4c3252dfa2b284f22cf2
Mark Malisew <markmalischew@gmail.com>
2026-09-06 17:44:37 +0700

    Первый коммит: меню и функция варианта 4

---
+++ main.cpp
@@ -0,0 +1,41 @@
+// Лабораторная работа № 1. Вариант 4.
+// Выполнил: Малышев М. А., группа ПИ-52.
+#include <iostream>
+
+using namespace std;
+
+// ===== Расчётные функции варианта =====
+
+// Площадь треугольника со стороной a и высотой h
+double triangleArea(double a, double h) {
+    return a * h / 2;
+}
+
+// ===== Главная функция: меню =====
+int main() {
+    int choice;
+    double a, b, c, h;
+
+    do {
+        cout << "\n=== Вариант 4: расчёты для треугольника ===\n";
+        cout << "1. Площадь\n";
+	cout << "0. Выход\n";
+        cout << "Выберите пункт: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                cout << "Введите сторону a и высоту h: ";
+                cin >> a >> h;
+                cout << "Площадь = " << triangleArea(a, h) << "\n";
+                break;
+            case 0:
+                cout << "Работа завершена.\n";
+                break;
+            default:
+                cout << "Такого пункта нет.\n";
+        }
+    } while (choice != 0);
+
+    return 0;
+}
